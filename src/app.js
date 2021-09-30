f868117877d0780c7db059438d37f20c99fb173b
Jodie <88211843+JodieOrnelas@users.noreply.github.com>
2021-09-30 16:04:19 -0500

    Finished API Integration

--- src/app.js
+++ src/app.js
@@ -1,20 +1,59 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (hours > 12) {
+    hours -= 12;
+  } else if (hours === 0) {
+    hours = 12;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayInfo(response) {
+  console.log(response);
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#tempNow");
   let descriptionElement = document.querySelector("#description");
-  //  let humidityElement = document.querySelector("#humidity");
+  let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  // let dateElement = document.querySelector("#date");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  //humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  //dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
-
+let city = "New York";
 let apiKey = "427140810191e99fb93ea0eb8a6cf6df";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(url).then(displayInfo);
