da2256e393db25c0b7def17ad536b157d2a73e4b
Jodie <88211843+JodieOrnelas@users.noreply.github.com>
2021-09-30 16:21:09 -0500

    Added Icon Feature

--- src/app.js
+++ src/app.js
@@ -44,6 +44,7 @@ function displayInfo(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -51,6 +52,11 @@ function displayInfo(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let city = "New York";
 let apiKey = "427140810191e99fb93ea0eb8a6cf6df";
